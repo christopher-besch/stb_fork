772632e85b29baba221d6a0ebbee4294ca61f73e
Christopher Besch <christopher.besch@gmx.de>
2021-05-24 12:25:39 +0200

    updated readme

--- premake5.lua
+++ premake5.lua
@@ -8,13 +8,10 @@ project "stb"
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    filter ""
-
     -- all files that are included or compiled = all files that are visible to the preprocessor/compiler/linker and make and stuff
     files
     {
-        "**.h",
-        "**.cpp"
+        "**.h"
     }
 
     -- for #include paths with ""
